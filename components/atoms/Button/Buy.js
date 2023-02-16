443979d5a951e7aeeff78433e91ff98b60c64b92
Peter NDENGO <frdrcpeter@gmail.com>
2023-02-16 07:09:42 +0100

    Buy

--- components/atoms/Button/Buy.js
+++ components/atoms/Button/Buy.js
@@ -2,12 +2,14 @@ import { HiOutlinePlus } from "react-icons/hi";
 
 const ButtonBuy = () => {
     return (
-        <button className='flex items-center gap-2 justify-between rounded-lg bg-orange py-2 px-3 effect-up shadow'>
-            <span className='bg-white text-sky p-1 rounded-md'>
-                <HiOutlinePlus />
-            </span>
-            <span className='text-sm font-light'>Buy now</span>
-        </button>
+        <>
+            <button htmlFor="my-modal-6" className='flex items-center gap-2 justify-between rounded-lg bg-orange py-2 px-3 effect-up shadow'>
+                <span className='bg-white text-sky p-1 rounded-md'>
+                    <HiOutlinePlus />
+                </span>
+                <span className='text-sm font-light'>Buy now</span>
+            </button>
+        </>
     );
 }
 
