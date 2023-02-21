0298f782c497ec0ef225d12ff6a5d2f36515dcd2
Peter NDENGO <frdrcpeter@gmail.com>
2023-02-21 10:12:19 +0100

    Script

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ export default function App({ Component, pageProps }) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/favicon.png" />
+        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
       </Head>
       <SessionProvider session={pageProps.session}>
         <ThemeProvider theme={theme}>        
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ const Page = () => {
     <>
       <Head>
         <title>JAMII - UNICEF CRYPTO</title>
+        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
       </Head>
       <VoucherHome />
       <ProviderFilter />
