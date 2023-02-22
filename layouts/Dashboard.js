9b4c1f4c89bd8b747ef5cbbaa3f70f0b82023480
Peter NDENGO <frdrcpeter@gmail.com>
2023-02-22 11:23:33 +0100

    Format doc

--- layouts/Dashboard.js
+++ layouts/Dashboard.js
@@ -5,33 +5,33 @@ import Sidebar from "../components/molecules/Sidebar/Index";
 import { useSession, signOut } from "next-auth/react";
 
 const Dashboard = ({ children, className }) => {
-  const { pathname } = useRouter();
-  const { data: session } = useSession();
+	const { pathname } = useRouter();
+	const { data: session } = useSession();
 
-  const handleSignOut = () => {
-    signOut();
-  };
+	const handleSignOut = () => {
+		signOut();
+	};
 
-  return (
-    <>
-      {!session ? (
-        <>
-          <Menu session={session} handleSignOut={handleSignOut} />
-          <main className="min-h-[90vh] grid md:grid-cols-5 gap-6 mt-20">
-            <Sidebar activePath={pathname} />
-            <div
-              className={`${className} col-span-4 p-4 overflow-x-hidden bg-[#f0f4fd]`}
-            >
-              {children}
-            </div>
-          </main>
-          <MenuBottom activePath={pathname} />
-        </>
-      ) : (
-        <></>
-      )}
-    </>
-  );
+	return (
+		<>
+			{!session ? (
+				<>
+					<Menu session={session} handleSignOut={handleSignOut} />
+					<main className="min-h-[90vh] grid md:grid-cols-5 gap-6 mt-20">
+						<Sidebar activePath={pathname} />
+						<div
+							className={`${className} col-span-4 p-4 overflow-x-hidden bg-[#f0f4fd]`}
+						>
+							{children}
+						</div>
+					</main>
+					<MenuBottom activePath={pathname} />
+				</>
+			) : (
+				<></>
+			)}
+		</>
+	);
 };
 
 export default Dashboard;
