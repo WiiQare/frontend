f6b3eb9e4fc7964d8c814c065262c8d0a90ee0e0
Peter NDENGO <frdrcpeter@gmail.com>
2023-02-24 08:54:50 +0100

    Form repeater

--- components/atoms/Modal/content.js
+++ components/atoms/Modal/content.js
@@ -1,8 +1,29 @@
 import React, { Fragment, useState } from "react";
-import { Dialog, Transition } from '@headlessui/react'
+import { Dialog, Transition } from '@headlessui/react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import { HiXMark } from "react-icons/hi2";
 
 
-const ContentModal = ({ title, children }) => {
+const TabHistories = [
+	{
+		name: "email",
+	},
+	{
+		name: "Phone",
+	}
+]
+
+const ContentModal = ({ title, tabs, children }) => {
+	const [value, setValue] = useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
 	return (
 		<div className="fixed inset-0 overflow-y-auto">
 			<div className="flex items-center justify-center min-h-full p-4 text-center">
@@ -16,15 +37,20 @@ const ContentModal = ({ title, children }) => {
 					leaveTo="opacity-0 scale-95"
 				>
 					<Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl space-y-8">
-						<Dialog.Title as="h3" className="text-xl font-semibold leading-6 text-gray-900">
-							{title}
+						<Dialog.Title as="div" className="flex justify-between items-center">
+							<h3 className="text-md font-semibold leading-6 text-gray-900">
+								{title}
+							</h3>
+
+							{tabs ? <TabsModal value={value} handleChange={handleChange} /> : <></>}
 						</Dialog.Title>
 
-						<div className="mt-2">
-							<div className="text-sm text-gray-500">
-								{children}
-							</div>
-						</div>
+
+						{
+							tabs ? (
+								<TabItems value={value} />
+							) : <></>
+						}
 
 					</Dialog.Panel>
 				</Transition.Child>
@@ -34,3 +60,198 @@ const ContentModal = ({ title, children }) => {
 }
 
 export default ContentModal;
+
+function TabItems({ value }) {
+	// For Email
+	const [allFriends, setAllFriends] = useState([
+		{ email: "" },
+	]);
+
+	const [allFriendsPhone, setAllFriendsPhone] = useState([
+		{ phone: "" },
+	]);
+
+	const handleAddFriend = (phone = false) => {
+		if (phone) {
+			const values = [...allFriendsPhone];
+			values.push({
+				email: "",
+			});
+			setAllFriendsPhone(values);
+		} else {
+			const values = [...allFriends];
+			values.push({
+				email: "",
+			});
+			setAllFriends(values);
+		}
+	};
+
+	const handleInputChange = (index, event, phone = false) => {
+		if (phone) {
+			const values = [...allFriendsPhone];
+			const updatedValue = event.target.name;
+			values[index][updatedValue] = event.target.value;
+
+			setAllFriendsPhone(values);
+		} else {
+
+			const values = [...allFriends];
+			const updatedValue = event.target.name;
+			values[index][updatedValue] = event.target.value;
+
+			setAllFriends(values);
+		}
+	};
+
+	const handleRemoveFriends = (index, phone = false) => {
+		if (phone) {
+
+			const values = [...allFriendsPhone];
+			if (values.length > 1) {
+				values.splice(index, 1);
+				setAllFriendsPhone(values);
+			}
+		} else {
+			const values = [...allFriends];
+			if (values.length > 1) {
+				values.splice(index, 1);
+				setAllFriends(values);
+			}
+		}
+	};
+
+	//console.log(allFriends);
+
+	return (
+		<div className="mt-2">
+			{/* For Email */}
+			<div className="text-sm text-gray-500">
+				<TabPanel value={value} index={0} >
+					<div className="space-y-8">
+
+						<div className="flex flex-col gap-2 w-full">
+							{
+								allFriends.map((field, index) => (
+									<form key={index} className="flex w-full justify-between items-center gap-3">
+										<input
+											className="w-9/12 py-3 placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+											type="email"
+											name="email"
+											placeholder="Enter email address"
+											value={field.email}
+											onChange={(event) =>
+												handleInputChange(index, event)
+											}
+										/>
+										{
+											index > 0 ? (
+												<button className="bg-red-300 rounded-full hover:bg-red-700 hover:text-white transition duration-200" onClick={() => handleRemoveFriends(index)}>
+													<HiXMark />
+												</button>
+											) : <></>
+										}
+									</form>
+								))
+							}
+						</div>
+
+						<div className="flex flex-row-reverse gap-4">
+							<button className="bg-orange text-sm py-2 px-4 rounded-lg effect-up text-white" onClick={() => handleAddFriend()}>
+								+ Add Friend
+							</button>
+							<button className="border border-orange text-sm py-2 px-4 rounded-lg text-orange effect-up">Submit</button>
+						</div>
+					</div>
+
+				</TabPanel>
+
+				<TabPanel value={value} index={1} >
+
+					<div className="space-y-8">
+
+						<div className="flex flex-col gap-2 w-full">
+							{
+								allFriendsPhone.map((field, index) => (
+									<form key={index} className="flex w-full justify-between items-center gap-3">
+										<input
+											className="w-9/12 py-3 placeholder:text-gray-400 hover:outline-none focus:ring-0 border border-gray-300 rounded-lg focus:ring-sky"
+											type="text"
+											name="Phone Number"
+											placeholder="Enter your phone number"
+											value={field.phone}
+											onChange={(event) =>
+												handleInputChange(index, event)
+											}
+										/>
+										{
+											index > 0 ? (
+												<button className="bg-red-300 rounded-full hover:bg-red-700 hover:text-white transition duration-200" onClick={() => handleRemoveFriends(index, true)}>
+													<HiXMark />
+												</button>
+											) : <></>
+										}
+									</form>
+								))
+							}
+						</div>
+
+						<div className="flex flex-row-reverse gap-4">
+							<button className="bg-orange text-sm py-2 px-4 rounded-lg effect-up text-white" onClick={() => handleAddFriend(true)}>
+								+ Add Friend
+							</button>
+							<button className="border border-orange text-sm py-2 px-4 rounded-lg text-orange effect-up">Submit</button>
+						</div>
+					</div>
+
+				</TabPanel>
+			</div>
+		</div>
+	)
+}
+
+function TabsModal({ value, handleChange }) {
+
+	return (
+		<div>
+			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+				<Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+					{TabHistories.map((item, index) => <Tab label={item.name} {...a11yProps(index)} key={index} />)}
+				</Tabs>
+			</Box>
+		</div>
+	)
+}
+
+export function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
+}
--- components/organisms/Dashboard/Voucher.js
+++ components/organisms/Dashboard/Voucher.js
@@ -2,6 +2,7 @@ import { CiCircleInfo } from "react-icons/ci";
 import { HiUserGroup } from "react-icons/hi";
 import ButtonEarn from "../../atoms/Button/Earn";
 import WithQR from "../../atoms/Card/Debit/withQR";
+import { TabPanel } from "../../atoms/Modal/content";
 
 const VoucherHome = () => {
 	return (
@@ -14,10 +15,7 @@ const VoucherHome = () => {
 				<HiUserGroup size={60} className="text-sky" />
 				<div className="space-y-3 text-center w-full">
 					<h1 className="text-gray-800 text-lg">Invite your friends</h1>
-					<ButtonEarn modal={{title: "Invite your friends"}}>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quos atque voluptatum deserunt nostrum, vel, eligendi alias odio quasi ullam dolorem in sapiente nobis voluptas! Cum dolores quas rerum ex?
-						Repudiandae dicta suscipit repellendus assumenda aut soluta quod nobis, quaerat placeat illo eos distinctio quam rem, explicabo deserunt nemo. Minima, quis numquam ex eveniet inventore quo atque animi voluptas soluta!
-					</ButtonEarn>
+					<ButtonEarn modal={{ title: "Invite your friends" }}></ButtonEarn>
 					<span data-popover-target="popover-description" className="absolute top-1 right-6 cursor-pointer text-gray-700 md:text-gray-400"><CiCircleInfo size={25} /></span>
 
 					<div data-popover id="popover-description" role="tooltip" class="absolute z-50 invisible inline-block text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 ">
@@ -26,7 +24,7 @@ const VoucherHome = () => {
 							<p>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque hic sunt nemo fugiat porro, exercitationem commodi velit dolor eos nam sint sapiente iure maxime vitae nisi error autem tempora.
 							</p>
-							
+
 							<a href="#" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700">Read more <svg class="w-4 h-4 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></a>
 						</div>
 						<div data-popper-arrow></div>
