d6377601e450ec0966e050b4d1666dd439a6db2c
Peter NDENGO <frdrcpeter@gmail.com>
2023-03-21 09:24:57 +0100

    Add button sign with social media

--- components/organisms/Auth/LoginForm.js
+++ components/organisms/Auth/LoginForm.js
@@ -10,105 +10,124 @@ import Stack from "@mui/material/Stack";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { signIn } from "next-auth/react"
 
 import OnboardingScreen from "../../molecules/OnboardingScreen";
 import { useLocalStorage } from "../../../hooks/useLocalStorage";
 import {
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  Typography,
+	FormControl,
+	IconButton,
+	InputAdornment,
+	InputLabel,
+	OutlinedInput,
+	Typography,
 } from "@mui/material";
 import MenuHolder from "../../atoms/MenuHolder";
 import BlinkSnackbar from "../../atoms/BlinkSnackbar";
+import Image from "next/image";
 
 function LoginForm() {
-  const matches = useMediaQuery("(max-width: 992px)");
-  const [onboardingIsEnd, setOnboardingIsEnd] = React.useState("");
-  const [showPassword, setShowPassword] = React.useState(false);
+	const matches = useMediaQuery("(max-width: 992px)");
+	const [onboardingIsEnd, setOnboardingIsEnd] = React.useState("");
+	const [showPassword, setShowPassword] = React.useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  React.useEffect(() => {
-    setOnboardingIsEnd(localStorage.getItem("onboardingIsEnd") || "");
-  }, []);
+	React.useEffect(() => {
+		setOnboardingIsEnd(localStorage.getItem("onboardingIsEnd") || "");
+	}, []);
 
-  const formHolder = (
-    <>
-      <div className="form-holder">
-        <MenuHolder href="/register" label="SIGN UP" />
-        <div className="signin-signup-form">
-          <div className="form-items">
-            <div className="form-title">Sign in to your account</div>
-            <Box sx={{ mb: 3, mt: 2 }}></Box>
-            <form id="signinform">
-              <Stack spacing={2}>
-                <TextField
-                  id="outlined-basic"
-                  fullWidth
-                  label="E-mail Address"
-                  variant="outlined"
-                />
-                <FormControl fullWidth variant="outlined">
-                  <InputLabel htmlFor="outlined-basic1">Password</InputLabel>
-                  <OutlinedInput
-                    id="outlined-basic1"
-                    label="Password"
-                    type={showPassword ? "text" : "password"}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                  />
-                </FormControl>
-                <Box sx={{ textAlign: "right", mb: 2 }}>
-                  <Typography color="text.secondary" variant="caption">
-                    Forgot your password ?
-                  </Typography>
-                </Box>
-                <Box>
-                  <Button
-                    size="large"
-                    variant="contained"
-                    onClick={() => {}}
-                    className="bg-yellow"
-                  >
-                    SIGN IN
-                  </Button>
-                </Box>
-              </Stack>
-            </form>
-          </div>
-        </div>
-      </div>
-      <BlinkSnackbar />
-    </>
-  );
-  return (
-    <>
-      {matches && !onboardingIsEnd ? (
-        <OnboardingScreen
-          onStartClick={() => {
-            if (typeof window !== "undefined") {
-              localStorage.setItem("onboardingIsEnd", "onboardingIsEnd");
-            }
-            setOnboardingIsEnd("onboardingIsEnd");
-          }}
-        />
-      ) : (
-        formHolder
-      )}
-    </>
-  );
+	//Handle Google Login
+    const handleGoogleSignIn = async () => {
+        signIn('google', {callbackUrl: "http://localhost:3000"})
+    }
+
+	const formHolder = (
+		<>
+			<div className="form-holder">
+				<MenuHolder href="/register" label="SIGN UP" />
+				<div className="signin-signup-form">
+					<div className="flex flex-col w-3/4 border-opacity-50 gap-4">
+						
+						<div className="form-items !w-full !max-w-full flex flex-col items-center mx-auto">
+							<div className="w-3/4 mx-auto">
+								<div className="form-title">Sign in to your account</div>
+								<Box sx={{ mb: 3, mt: 2 }}></Box>
+								<form id="signinform" className="">
+									<Stack spacing={2}>
+										<TextField
+											id="outlined-basic"
+											fullWidth
+											label="E-mail Address"
+											variant="outlined"
+										/>
+										<FormControl fullWidth variant="outlined">
+											<InputLabel htmlFor="outlined-basic1">Password</InputLabel>
+											<OutlinedInput
+												id="outlined-basic1"
+												label="Password"
+												type={showPassword ? "text" : "password"}
+												endAdornment={
+													<InputAdornment position="end">
+														<IconButton
+															aria-label="toggle password visibility"
+															onClick={handleClickShowPassword}
+															edge="end"
+														>
+															{showPassword ? <VisibilityOff /> : <Visibility />}
+														</IconButton>
+													</InputAdornment>
+												}
+											/>
+										</FormControl>
+										<Box sx={{ textAlign: "right", mb: 2 }}>
+											<Typography color="text.secondary" variant="caption">
+												Forgot your password ?
+											</Typography>
+										</Box>
+										<Box>
+											<Button
+												size="large"
+												variant="contained"
+												onClick={() => { }}
+												className="bg-yellow"
+											>
+												SIGN IN
+											</Button>
+										</Box>
+									</Stack>
+								</form>
+							</div>
+						</div>
+
+						<div className="divider"><span className="text-gray-400">OR</span></div>
+
+						<div className="flex flex-col md:flex-row gap-4 mb-2">
+							<button type="button" className="w-full py-3 flex justify-center items-center gap-4 hover:bg-gray-200 rounded-xl border font-medium" onClick={handleGoogleSignIn}><Image src={"/images/google.svg"} width="20" height="20" alt="Google logo" /> Sign In with Google</button>
+							<button type="button" className="w-full py-3 flex justify-center items-center gap-4 hover:bg-gray-200 rounded-xl border font-medium" onClick={() => null}><Image src={"/images/facebook.png"} width="25" height="25" alt="Facebook logo" /> Sign In with Facebook</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<BlinkSnackbar />
+		</>
+	);
+	return (
+		<>
+			{matches && !onboardingIsEnd ? (
+				<OnboardingScreen
+					onStartClick={() => {
+						if (typeof window !== "undefined") {
+							localStorage.setItem("onboardingIsEnd", "onboardingIsEnd");
+						}
+						setOnboardingIsEnd("onboardingIsEnd");
+					}}
+				/>
+			) : (
+				formHolder
+			)}
+		</>
+	);
 }
 
 export default LoginForm;
