c24f89f1422ae8de20b5a2b5c32c42d63a7ed104
Peter NDENGO <frdrcpeter@gmail.com>
2023-03-29 07:20:33 +0100

    Page pass

--- pages/voucher/pass/[pass].js
+++ pages/voucher/pass/[pass].js
@@ -1,26 +1,23 @@
-import React, { useContext, useState } from "react";
+import { useState } from "react";
 import { useQRCode } from "next-qrcode";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { FormContext } from "../../../../pages/voucher/buy";
 import Image from "next/image";
-import logoDark from "../../../../public/images/logo_dark_2.png";
-import { HiOutlineClipboardDocument } from "react-icons/hi2";
+import logoDark from "../../../public/images/logo_dark.png";
+import logo from "../../../public/images/logo.png";
 
 
-function Send() {
+function Pass() {
 	const { Canvas } = useQRCode();
-	const { activeStepIndex, setActiveStepIndex, formData, setFormData } = useContext(FormContext);
 	const [copy, setCopy] = useState(false);
 
-
 	return (
-		<div className="flex flex-col gap-6 justify-center items-center">
+		<div className="flex flex-col gap-6 justify-center items-center h-screen w-screen overflow-hidden">
 			<div className="flex flex-col items-center text-center space-y-2">
 				<div className="flex flex-col items-center select-none">
-					<Image
-						src={logoDark}
-						className="h-6 md:h-9 object-left object-contain w-min"
-					/>
+                    <Image
+                        src={logoDark}
+                        className="h-6 md:h-9 object-left object-contain w-min"
+                    />
 					<h1 className="font-extrabold text-gray-700 text-lg hidden md:flex">Pass Sante</h1>
 				</div>
 				<span className="text-xs flex items-center gap-1">Pass Sante ID:
@@ -75,25 +72,8 @@ function Send() {
 				</div>
 			</div>
 
-			<div className="text-center mt-6 space-y-2">
-				<h4 className="font-semibold text-gray-700 text-sm">Envoyer le pass santé au bénéficiaire:</h4>
-				<div className="flex justify-between">
-					<button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center gap-2 mr-2 mb-2">
-						<img src="/images/whatsapp.png" alt="" className="w-6" />
-						WhatsApp
-					</button>
-					<button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center gap-2 mr-2 mb-2">
-						<img src="/images/facebook-share.png" alt="" className="w-6" />
-						Facebook
-					</button>
-					<button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center gap-2 mr-2 mb-2">
-						<img src="/images/share.png" alt="" className="w-6" />
-						Message
-					</button>
-				</div>
-			</div>
 		</div>
 	);
 }
 
-export default Send;
+export default Pass;
