f1e22104765b9e4f01cd020a63f5e8b352d3d98f
Peter NDENGO <frdrcpeter@gmail.com>
2023-03-29 07:33:49 +0100

    Secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ export default NextAuth({
         //Google Provider
         GoogleProvider({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
 
         //LinkedIn Provider
