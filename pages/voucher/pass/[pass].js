ab23a6e5b0a60b20eb54da33166e1c58c45b2f4a
Peter NDENGO <frdrcpeter@gmail.com>
2023-04-22 09:02:12 +0100

    Pass voucher

--- pages/voucher/pass/[pass].js
+++ pages/voucher/pass/[pass].js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Fetcher from "../../../lib/Fetcher";
 import { HiExclamation } from "react-icons/hi";
 import { useRouter } from 'next/router'
-
+import CurrencyFlag from "react-currency-flags";
 
 function Pass() {
 	const router = useRouter()
@@ -100,7 +100,14 @@ function Pass() {
 							<img className="inline-block h-[2.875rem] w-[2.875rem] rounded-full ring-2 ring-white dark:ring-gray-800" src="/images/femme.png" alt="Image Description" />
 						</div>
 
-						<h4 className="text-sm text-center"><span className="font-semibold">{data.currency == "usd" ? "$" : data.currency}{data.amount}</span> Health Pass WiiQare <br /> From <span className="text-orange font-semibold"><SliceText text={data.senderId}/></span> To <span className="text-orange font-semibold"><SliceText text={data.voucher.patientId} /></span></h4>
+						
+						<h4 className="text-sm text-center">
+						<span className="flex items-center justify-center gap-2">
+							<span className="font-semibold flex items-center gap-1">
+								<CurrencyFlag currency={data.currency} className="rounded-full !h-4 !w-4 object-cover" /> {new Intl.NumberFormat("en-US", {style: 'currency', currency: data.currency}).format(data.amount)}</span> 
+								Pass santé WiiQare 
+							</span>
+							de <span className="text-orange font-semibold">{data.sender.firstName}</span> à <span className="text-orange font-semibold">{data.patient.firstName}</span></h4>
 					</div>
 				</div>
 			</>
