fa15797261290a934b852b7c53ccdb365a8173ef
Peter NDENGO <frdrcpeter@gmail.com>
2023-05-03 15:02:08 +0100

    Send Message

--- components/molecules/Navbar/Drawer.js
+++ components/molecules/Navbar/Drawer.js
@@ -4,6 +4,11 @@ import { BiArrowBack } from "react-icons/bi";
 export const ChatContext = createContext();
 import { useSession } from "next-auth/react";
 import Fetcher from "../../../lib/Fetcher";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useMutation } from "react-query";
+import { sendMessage } from "../../../lib/helper";
+
 
 export default function Drawer() {
     const [personalChat, setPersonalChat] = useState({ state: true });
@@ -32,7 +37,7 @@ export default function Drawer() {
                 <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
 
                 {
-                    !personalChat.state ? <ListItemChat handleChange={() => handleChange()} /> : <Conversation close={() => setDraw(false)} messages={allMessages} isLoading={isLoading} isError={isError} user={session.user.data.names} />
+                    !personalChat.state ? <ListItemChat handleChange={() => handleChange()} /> : <Conversation close={() => setDraw(false)} messages={allMessages} isLoading={isLoading} isError={isError} user={session.user.data.names} senderId={session.user.data.userId} token={session.user.data.access_token} />
                 }
 
 
@@ -61,9 +66,42 @@ function ItemMessage({ name, message, onClick }) {
     )
 }
 
-function Conversation({ close, messages, isLoading, isError, user }) {
+function Conversation({ close, setMessages, messages, isLoading, isError, user, senderId, token }) {
 
-    console.log(messages);
+    const [text, setText] = useState('');
+    const sendMessageMutation = useMutation(sendMessage, {
+        onSuccess: (res) => {
+
+            console.log(res);
+            if (res.code) {
+
+                //set error
+
+            } else {
+                // Set success
+                setText('')
+                messages.push(res)
+                setMessages(messages)
+            };
+        }
+    });
+
+    const onSubmit = async (values) => {
+
+        sendMessageMutation.mutate({ message: text, senderId, accessToken: token })
+    };
+
+    const ValidationSchema = yup.object().shape({
+		message: yup.string().required("Message ne doit pas être vide"),
+	});
+
+    // Formik hook
+	const formik = useFormik({
+		initialValues: {
+			message: ''
+		},
+		onSubmit
+	})
 
     return (
         <div className="flex flex-col bg-gray-100 flex-auto md:w-96 w-full h-full relative">
@@ -156,62 +194,23 @@ function Conversation({ close, messages, isLoading, isError, user }) {
                                         ) : <>Démarrer un conversation avec notre équipe...</>
                                     }
 
-
-                                    {/* <div className="col-start-6 col-end-13 p-3 rounded-lg">
-                                        <div className="flex items-center justify-start flex-row-reverse">
-                                            <div
-                                                className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                                            >
-                                                B
-                                            </div>
-                                            <div
-                                                className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
-                                            >
-                                                <div>I'm ok what about you?</div>
-                                            </div>
-                                        </div>
-                                    </div> */}
-
                                 </div>
                             </div>
                         </div>
                     )
                 }
 
-                <div
-                    className="flex flex-row items-center h-16 bg-white w-full px-4 absolute bottom-40 md:bottom-20"
-                >
-                    <div>
-                        <button
-                            className="flex items-center justify-center text-gray-400 hover:text-gray-600"
-                        >
-                            <svg
-                                className="w-5 h-5"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                                ></path>
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="flex-grow ml-4">
-                        <div className="relative w-full">
-                            <input
-                                type="text"
-                                className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
-                            />
+				<form id="sendMessageform" onSubmit={formik.handleSubmit}>
+            
+                    <div
+                        className="flex flex-row items-center h-16 bg-white w-full px-4 absolute bottom-40 md:bottom-20"
+                    >
+                        <div>
                             <button
-                                className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
+                                className="flex items-center justify-center text-gray-400 hover:text-gray-600"
                             >
                                 <svg
-                                    className="w-6 h-6"
+                                    className="w-5 h-5"
                                     fill="none"
                                     stroke="currentColor"
                                     viewBox="0 0 24 24"
@@ -221,35 +220,65 @@ function Conversation({ close, messages, isLoading, isError, user }) {
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
                                     ></path>
                                 </svg>
                             </button>
                         </div>
-                    </div>
-                    <div className="ml-4">
-                        <button
-                            className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-3 h-fit flex-shrink-0"
-                        >
-                            <span className="ml-2">
-                                <svg
-                                    className="w-4 h-4 transform rotate-45 -mt-px"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
+                        <div className="flex-grow ml-4">
+                            <div className="relative w-full">
+                                <input
+                                    type="text"
+                                    name="message"
+                                    value={text}
+                                    onChange={(e) => { setText(e.target.value) }}
+                                    className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                                />
+                                <button
+                                    className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
                                 >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                                    ></path>
-                                </svg>
-                            </span>
-                        </button>
+                                    <svg
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="ml-4">
+                            <button
+                                type="submit"
+                                className={`flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-3 h-fit flex-shrink-0 disabled:bg-gray-400`} disabled={!text ? true : false}
+                            >
+                                <span className="ml-2">
+                                    <svg
+                                        className="w-4 h-4 transform rotate-45 -mt-px"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                        ></path>
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
