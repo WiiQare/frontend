83de5a42f3fa0a56bb4b1bb8b3ec304ac3d0ae3f
Abhi Shandilya <abhiShandy@users.noreply.github.com>
2023-05-12 11:46:06 -0400

    test: setup jest (#8)

---
+++ tests/pages/404.test.js
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import Page from "@/pages/404";
+import { render, screen } from "@testing-library/react";
+
+describe("404 page", () => {
+  it("should render the page", () => {
+    render(<Page />);
+    expect(screen.getByText("You seem to be lost!")).toBeInTheDocument();
+  });
+});
