390276c061e8e7c8b5ea0fb5b919f9135de76340
abhishandy <abhishek.sandy@gmail.com>
2023-05-13 00:11:37 +0530

    test: progress card

---
+++ components/atoms/Card/Progress/index.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Progress from ".";
+
+describe("Progress", () => {
+  it("should render the component", () => {
+    render(<Progress />);
+    expect(screen.getByRole("progressbar")).toBeInTheDocument();
+  });
+
+  it("should render the value", () => {
+    const value = 50;
+    render(<Progress value={value} />);
+    expect(screen.getByText(`${value}%`)).toBeInTheDocument();
+  });
+});
