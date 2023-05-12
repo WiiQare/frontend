2ff97f1a99ea80fe7611a6fd6b7b5cd1b7721761
abhishandy <abhishek.sandy@gmail.com>
2023-05-13 00:03:38 +0530

    test: noAction button

---
+++ components/atoms/Button/NoAction.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import NoAction from "./NoAction";
+
+describe("NoAction button", () => {
+  it("should render the component", () => {
+    render(<NoAction />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should render the text", () => {
+    const text = "Button";
+    render(<NoAction text={text} />);
+    expect(screen.getByText(text)).toBeInTheDocument();
+  });
+});
