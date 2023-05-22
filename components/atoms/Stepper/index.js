f1e432403042cba00e499aad4476db42767b3ffc
abhishandy <abhishek.sandy@gmail.com>
2023-05-22 18:27:52 -0400

    test: stepper index

--- components/atoms/Stepper/index.js
+++ components/atoms/Stepper/index.js
@@ -2,49 +2,54 @@ import React, { useContext, useEffect } from "react";
 import { FormContext } from "../../../pages/voucher/buy";
 
 function Stepper() {
+  const { activeStepIndex } = useContext(FormContext);
 
-    const { activeStepIndex } = useContext(FormContext);
+  useEffect(() => {
+    const stepperItems = document.querySelectorAll(".stepper-item");
+    stepperItems.forEach((step, i) => {
+      if (i <= activeStepIndex) {
+        step.classList.add("bg-primary", "text-white");
 
-    useEffect(() => {
-        const stepperItems = document.querySelectorAll(".stepper-item");
-        stepperItems.forEach((step, i) => {
-            if (i <= activeStepIndex) {
-                step.classList.add("bg-primary", "text-white");
+        if (i < activeStepIndex)
+          step.nextSibling.classList.add("border-primary");
+      } else {
+        step.classList.remove("bg-primary", "text-white");
+      }
+    });
+  }, [activeStepIndex]);
 
-                if(i < activeStepIndex) step.nextSibling.classList.add('border-primary')
-
-            } else {
-                step.classList.remove("bg-primary", "text-white");
-            }
-        });
-    }, [activeStepIndex]);
-
-    return (
-        <div className="w-full md:w-2/3 flex flex-row items-center justify-center py-4 mb-14 md:mb-16">
-            <div className="flex flex-col relative">
-                <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
-                    1
-                </div>
-                <span className="absolute -bottom-6 text-xs w-max md:-left-8">Identité <span className="hidden md:inline">du beneficiare</span> </span>
-            </div>
-
-            <div className="flex-auto border-t-2"></div>
-
-            <div className="flex flex-col relative">
-                <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
-                    2
-                </div>
-                <span className="absolute -bottom-6 text-xs w-max md:-left-8">Résumé <span className="hidden md:inline">de paiement</span> </span>
-            </div>
-            <div className="flex-auto border-t-2"></div>
-            <div className="flex flex-col relative">
-                <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
-                    3
-                </div>
-                <span className="absolute -bottom-6 text-xs w-max md:-left-8">Généré un <span className="hidden md:inline">Pass Santé </span> </span>
-            </div>
+  return (
+    <div className="w-full md:w-2/3 flex flex-row items-center justify-center py-4 mb-14 md:mb-16">
+      <div className="flex flex-col relative">
+        <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
+          1
         </div>
-    );
+        <span className="absolute -bottom-6 text-xs w-max md:-left-8">
+          Identité <span className="hidden md:inline">du beneficiare</span>{" "}
+        </span>
+      </div>
+
+      <div className="flex-auto border-t-2"></div>
+
+      <div className="flex flex-col relative">
+        <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
+          2
+        </div>
+        <span className="absolute -bottom-6 text-xs w-max md:-left-8">
+          Résumé <span className="hidden md:inline">de paiement</span>{" "}
+        </span>
+      </div>
+      <div className="flex-auto border-t-2"></div>
+      <div className="flex flex-col relative">
+        <div className="stepper-item flex items-center justify-center w-12 h-12 text-center font-medium border-2 rounded-full">
+          3
+        </div>
+        <span className="absolute -bottom-6 text-xs w-max md:-left-8">
+          Généré un <span className="hidden md:inline">Pass Santé </span>{" "}
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Stepper;
