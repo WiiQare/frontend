ad322ce7cf92e89bede73636ca0dab745ec9dc5c
abhishandy <abhishek.sandy@gmail.com>
2023-06-13 08:26:13 -0400

    country selector label

--- components/atoms/Input/Country.test.js
+++ components/atoms/Input/Country.test.js
@@ -1,13 +1,18 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Country from "./Country";
 import { CountryContext } from "../Stepper/Forms/identity2";
 
 describe("Country", () => {
-  let component;
+  let component, setCountry, setCountryLabel;
   beforeEach(() => {
+    setCountry = jest.fn();
+    setCountryLabel = jest.fn();
+
     const res = render(
-      <CountryContext.Provider value={{ country: "Indonesia" }}>
+      <CountryContext.Provider
+        value={{ country: "Indonesia", setCountry, setCountryLabel }}
+      >
         <Country />
       </CountryContext.Provider>
     );
@@ -16,5 +21,10 @@ describe("Country", () => {
 
   test("should render Country component", () => {
     expect(component).toMatchSnapshot();
+  });
+
+  test("should show the prompt to choose country", () => {
+    const label = screen.getByLabelText("Choisissez le pays");
+    expect(label).toBeInTheDocument();
   });
 });
