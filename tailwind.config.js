b93d1384478ea76db269657a303a4c394dddbea8
Peter Ndengo <Frdrcpeter@gmail.com>
2023-06-15 23:55:19 +0300

    Responsive 1/2 (#35)
    * Fix condition
    
    * Back put
    
    * fix button red
    
    * Fix CSS for splidejs
    
    * Fix session
    
    * Reduce minute session
    
    * Change Readme
    
    * Change documentatioon on english
    
    * Fix responsive homepage
    
    * Change .env file
    
    ---------
    
    Co-authored-by: bienvenu Zigabe <bienvenuzigabe544@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,26 @@ module.exports = {
         purple: "#A938D0"
       },
     },
+
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'tb': '900px',
+      // => @media (min-width: 900px) { ... }
+
+      'md': '980px',
+      // => @media (min-width: 980px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
     
   },
   plugins: [require("flowbite/plugin"), require("daisyui"), require("@headlessui/react"), require('preline/plugin')],
