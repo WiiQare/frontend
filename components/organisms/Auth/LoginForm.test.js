8f7a07d4d50ecabc074902de40f5a6f8181858dd
Abhi Shandilya <abhiShandy@users.noreply.github.com>
2023-06-24 12:31:11 -0400

    test: auth - login form (#41)

---
+++ components/organisms/Auth/LoginForm.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import LoginForm from "./LoginForm";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    router: [],
+  }),
+}));
+
+describe("LoginForm", () => {
+  it("renders", () => {
+    const { container } = render(
+      <QueryClientProvider client={new QueryClient()}>
+        <LoginForm />
+      </QueryClientProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
