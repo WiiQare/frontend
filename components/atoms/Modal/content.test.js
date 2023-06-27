31cacbfb569d714ad906b9182fecbebc2964225c
lukebrichey <lukebrichey@gmail.com>
2023-06-27 13:37:05 -0400

    test: Added tests for the test and tabs props in ContentModal

--- components/atoms/Modal/content.test.js
+++ components/atoms/Modal/content.test.js
@@ -1,22 +1,47 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ContentModal from "./content";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Dialog, Transition } from "@headlessui/react";
+
+global.IntersectionObserver = class IntersectionObserver {
+  observe() {
+    return null;
+  }
+  disconnect() {
+    return null;
+  }
+  unobserve() {
+    return null;
+  }
+};
 
 describe("ContentModal", () => {
   let component;
+  const queryClient = new QueryClient(); // Create a new QueryClient
   beforeEach(() => {
     const res = render(
-      <SessionProvider session={{ user: { name: "John Doe" } }}>
-        <Transition appear show={true}>
-          <Dialog onClose={() => {}}>
-            <ContentModal>
-              <div>Content</div>
-            </ContentModal>
-          </Dialog>
-        </Transition>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}> {/* Use QueryClientProvider here */}
+        <SessionProvider 
+          session={{ 
+            user: { 
+              name: "John Doe", 
+              data: { 
+                access_token: "mock_access_token" 
+              } 
+            } 
+          }}
+        >
+          <Transition appear show={true}>
+            <Dialog onClose={() => {}}>
+              <ContentModal title="Test Title" tabs={['email', 'Phone']}>
+                <div>Content</div>
+              </ContentModal>
+            </Dialog>
+          </Transition>
+        </SessionProvider>
+      </QueryClientProvider> // Close QueryClientProvider here
     );
     component = res.container;
   });
@@ -24,4 +49,13 @@ describe("ContentModal", () => {
   test("should render ContentModal component", () => {
     expect(component).toMatchSnapshot();
   });
+
+  test("should render ContentModal component with title", () => {
+    expect(screen.getByText("Test Title")).toBeInTheDocument();
+  });
+
+  test("should render ContentModal component with tabs", () => {
+    expect(screen.getByText("email")).toBeInTheDocument();
+    expect(screen.getByText("Phone")).toBeInTheDocument();
+  });  
 });
