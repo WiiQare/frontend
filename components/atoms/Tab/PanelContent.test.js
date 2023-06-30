c1c0bc32e134e0d4dd3f8a295bb598445f251aed
abhishandy <abhishek.sandy@gmail.com>
2023-06-30 13:35:04 -0400

    test: loading panel

--- components/atoms/Tab/PanelContent.test.js
+++ components/atoms/Tab/PanelContent.test.js
@@ -1,14 +1,17 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import PanelContent from "./PanelContent";
 import { TransactionContext } from "../../organisms/Transaction";
 import { SessionProvider } from "next-auth/react";
+import fetch from "node-fetch";
 
 describe("PanelContent", () => {
-  let component;
   beforeEach(() => {
     console.log = jest.fn();
-    const res = render(
+  });
+
+  it("should render the component", () => {
+    const { container } = render(
       <SessionProvider session={{ user: { data: { access_token: {} } } }}>
         <TransactionContext.Provider
           value={{ transaction: {}, setTransaction: () => {} }}
@@ -17,10 +20,26 @@ describe("PanelContent", () => {
         </TransactionContext.Provider>
       </SessionProvider>
     );
-    component = res.container;
+    expect(container).toMatchSnapshot();
   });
 
-  it("should render the component", () => {
-    expect(component).toMatchSnapshot();
+  it("should render loading panel", () => {
+    render(
+      <SessionProvider session={{ user: { data: { access_token: {} } } }}>
+        <TransactionContext.Provider
+          value={{ transaction: {}, setTransaction: () => {} }}
+        >
+          <PanelContent value={0} index={0} />
+        </TransactionContext.Provider>
+      </SessionProvider>
+    );
+
+    jest.spyOn(fetch, "default").mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.reject([]),
+      })
+    );
+
+    expect(screen.getAllByText("Chargement en cours...")).toHaveLength(1);
   });
 });
