66a60f594458fbdd4d0efe7c9c8fb3ec6ce7ae36
Peter NDENGO <frdrcpeter@gmail.com>
2023-07-03 17:54:59 +0100

    Fix design list

--- components/organisms/Dashboard/ProviderFilter.js
+++ components/organisms/Dashboard/ProviderFilter.js
@@ -53,7 +53,7 @@ const ProviderFilter = () => {
 	return (
 		<div className="px-4 space-y-4 pb-24 md:pb-4">
 			<h1 className="md:text-2xl text-gray-700 font-bold flex-wrap flex items-center gap-2 text-lg">
-				Les hôpitaux partenaires dans le pays{" "}
+				Les hôpitaux partenaires en{" "}
 				<Image src={drc} loading="lazy" className="w-6" alt="DRC" /> RD Congo
 			</h1>
 			{/* <div className="flex gap-2 flex-col md:flex-row md:gap-4 justify-between items-center">
