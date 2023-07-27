3677259d1abbfa6e0f77402406019dd41fb214a2
abhishandy <abhishek.sandy@gmail.com>
2023-07-27 19:33:05 -0400

    test: sending email using form

--- components/organisms/Auth/Forms/email.test.js
+++ components/organisms/Auth/Forms/email.test.js
@@ -1,14 +1,23 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Email from "./email";
 import { FormContextRegister } from "../RegisterForm";
 import { Provider } from "react-redux";
 import { store } from "../../../../redux/store";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { sendEmail } from "../../../../lib/helper";
+
+jest.mock("../../../../lib/helper", () => ({
+  sendEmail: jest.fn().mockResolvedValue({ code: 0 }),
+}));
 
 describe("Email", () => {
-  it("renders", () => {
+  let container;
+  beforeEach(() => {
+    jest.clearAllMocks();
     const queryClient = new QueryClient();
-    const { container } = render(
+    const res = render(
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
           <FormContextRegister.Provider
@@ -25,6 +34,36 @@ describe("Email", () => {
         </Provider>
       </QueryClientProvider>
     );
+    container = res.container;
+  });
+  it("renders", () => {
     expect(container).toMatchSnapshot();
+  });
+
+  it("renders the form", () => {
+    expect(container.querySelector("form")).toBeInTheDocument();
+  });
+
+  it("renders the email input", () => {
+    expect(screen.getByLabelText("E-mail Address")).toBeInTheDocument();
+  });
+
+  it("should send email", async () => {
+    const user = userEvent.setup();
+    const email = "test@example.com";
+    const emailInput = screen.getByLabelText("E-mail Address");
+    const form = container.querySelector("form");
+    const submitButton = screen.getByRole("button", {
+      name: "L'ÉTAPE SUIVANTE",
+    });
+    expect(submitButton).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+    expect(emailInput).toHaveValue("");
+    await user.type(emailInput, email);
+    expect(emailInput).toHaveValue(email);
+    await user.click(submitButton);
+    expect(sendEmail).toHaveBeenCalledTimes(1);
+    expect(sendEmail).toHaveBeenCalledWith({ email });
   });
 });
