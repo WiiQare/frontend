15751903375a5b1897f2db711f645d5fb8a751dc
abhishandy <abhishek.sandy@gmail.com>
2023-07-28 12:58:18 -0400

    test: checkout page

--- pages/checkout.js
+++ pages/checkout.js
@@ -7,7 +7,9 @@ import CheckoutForm from "../components/molecules/Stripe/CheckoutForm";
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
 
 export default function App() {
   const [clientSecret, setClientSecret] = React.useState("");
@@ -24,7 +26,7 @@ export default function App() {
   }, []);
 
   const appearance = {
-    theme: 'flat',
+    theme: "flat",
   };
   const options = {
     clientSecret,
