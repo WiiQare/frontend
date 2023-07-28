776fd64b1b4e0684ee8aad18f7d2d048aed32eb4
abhishandy <abhishek.sandy@gmail.com>
2023-07-27 20:39:10 -0400

    test: add beneficiary

--- components/atoms/Stepper/Forms/identity.test.js
+++ components/atoms/Stepper/Forms/identity.test.js
@@ -6,6 +6,7 @@ import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { store } from "../../../../redux/store";
 import { QueryClientProvider, QueryClient } from "react-query";
+import userEvent from "@testing-library/user-event";
 
 describe("Identity2", () => {
   let component;
@@ -31,5 +32,23 @@ describe("Identity2", () => {
 
   it("should render a text to add a beneficiary", () => {
     expect(screen.getByText("Ajouter un bénéficiaire")).toBeInTheDocument();
+  });
+
+  it("should fill the form", async () => {
+    const user = userEvent.setup();
+
+    const addBeneficiary = screen.getByText("Ajouter un bénéficiaire");
+
+    await user.click(addBeneficiary);
+
+    const phone = screen.getByLabelText("Numéro de Téléphone");
+    const firstName = screen.getByLabelText("Nom de famille");
+    const lastName = screen.getByLabelText("Prénom");
+    const email = screen.getByLabelText("Adresse e-mail (optional)");
+    const address = screen.getByLabelText("Adresse du domicile");
+    // const postalCode = screen.getByLabelText("Code postal");
+    const city = screen.getByLabelText("Ville");
+    // const country = screen.getByLabelText("Pays");
+    // const submit = screen.getByText("Continuer avec ce patient");
   });
 });
