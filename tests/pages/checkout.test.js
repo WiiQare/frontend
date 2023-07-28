7fd6473afac746d2bccb8d7ea583d7a72b3d544e
abhishandy <abhishek.sandy@gmail.com>
2023-07-28 19:37:59 -0400

    test: move tests

---
+++ tests/pages/checkout.test.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import Page from "@/pages/checkout";
+import { render } from "@testing-library/react";
+require("jest-fetch-mock").enableMocks();
+
+describe("checkout page", () => {
+  it("should render empty page when fetch fails", () => {
+    fetch.mockResponse("{}");
+    const { container } = render(<Page />);
+    expect(container).toMatchSnapshot();
+    expect(container.querySelector("div")).toBeEmptyDOMElement();
+  });
+});
---
+++ tests/pages/wallet/index.test.js
@@ -0,0 +1,19 @@
+import Wallet from "../../../pages/wallet";
+import { render } from "@testing-library/react";
+import { TransactionContext } from "../../../components/organisms/Transaction";
+import { SessionProvider } from "next-auth/react";
+
+describe("Wallet", () => {
+  it("should render the heading", () => {
+    const { container } = render(
+      <SessionProvider session={{ user: { data: { access_token: {} } } }}>
+        <TransactionContext.Provider
+          value={{ transaction: {}, setTransaction: () => {} }}
+        >
+          <Wallet />
+        </TransactionContext.Provider>
+      </SessionProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
