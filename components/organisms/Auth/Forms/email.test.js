fc3cf11b1663b8d6c1f091508ab9ef10e773f16f
Abhi Shandilya <abhiShandy@users.noreply.github.com>
2023-07-27 20:11:35 -0400

    test: sending email using form (#85)
    * test: sending email using form
    
    * cover sendEmail fail case

--- components/organisms/Auth/Forms/email.test.js
+++ components/organisms/Auth/Forms/email.test.js
@@ -1,14 +1,19 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Email from "./email";
 import { FormContextRegister } from "../RegisterForm";
 import { Provider } from "react-redux";
 import { store } from "../../../../redux/store";
 import { QueryClientProvider, QueryClient } from "react-query";
+import * as Helpers from "../../../../lib/helper";
 
 describe("Email", () => {
-  it("renders", () => {
+  let container;
+  beforeEach(() => {
+    jest.clearAllMocks();
     const queryClient = new QueryClient();
-    const { container } = render(
+    const res = render(
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
           <FormContextRegister.Provider
@@ -25,6 +30,55 @@ describe("Email", () => {
         </Provider>
       </QueryClientProvider>
     );
+    container = res.container;
+  });
+  it("renders", () => {
     expect(container).toMatchSnapshot();
+  });
+
+  it("renders the form", () => {
+    expect(container.querySelector("form")).toBeInTheDocument();
+  });
+
+  it("renders the email input", () => {
+    expect(screen.getByLabelText("E-mail Address")).toBeInTheDocument();
+  });
+
+  it("should send email", async () => {
+    jest.spyOn(Helpers, "sendEmail").mockResolvedValueOnce({});
+    const user = userEvent.setup();
+    const email = "test@example.com";
+    const emailInput = screen.getByLabelText("E-mail Address");
+    const form = container.querySelector("form");
+    const submitButton = screen.getByRole("button", {
+      name: "L'ÉTAPE SUIVANTE",
+    });
+    expect(submitButton).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(form).toBeInTheDocument();
+    expect(emailInput).toHaveValue("");
+    await user.type(emailInput, email);
+    expect(emailInput).toHaveValue(email);
+    await user.click(submitButton);
+    expect(Helpers.sendEmail).toHaveBeenCalledTimes(1);
+    expect(Helpers.sendEmail).toHaveBeenCalledWith({ email });
+  });
+
+  it("should handle error if email is not sent", async () => {
+    jest.spyOn(Helpers, "sendEmail").mockResolvedValueOnce({
+      code: 400,
+      message: "Email failed to send",
+    });
+    const user = userEvent.setup();
+    const email = "test@example.com";
+    const emailInput = screen.getByLabelText("E-mail Address");
+    const submitButton = screen.getByRole("button", {
+      name: "L'ÉTAPE SUIVANTE",
+    });
+
+    await user.type(emailInput, email);
+    await user.click(submitButton);
+    expect(Helpers.sendEmail).toHaveBeenCalledTimes(1);
+    expect(Helpers.sendEmail).toHaveBeenCalledWith({ email });
   });
 });
