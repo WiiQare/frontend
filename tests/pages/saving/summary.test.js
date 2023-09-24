f30f2bf206827d1832db20074cee2aa7e46c241c
abhishandy <abhishek.sandy@gmail.com>
2023-09-24 08:07:07 -0400

    test: fix summary

--- tests/pages/saving/summary.test.js
+++ tests/pages/saving/summary.test.js
@@ -3,21 +3,38 @@ import { render } from '@testing-library/react';
 import { TransactionContext } from '@/components/organisms/Transaction';
 import { SessionProvider } from 'next-auth/react';
 import { DrawContext } from '../../../pages/_app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 require('jest-fetch-mock').enableMocks();
 
 fetch.mockResponse('[]');
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    query: {
+      'payment-intent': 'pi_1J4JrjGswQjYFZwX0Z1Z1Z1Z',
+    },
+  }),
+}));
+
 describe('Saving Summary', () => {
   it('should render', () => {
+    const queryClient = new QueryClient();
     const { container } = render(
       <DrawContext.Provider
-        value={{ draw: {}, setDraw: () => {}, saving: { plan: {} } }}
+        value={{
+          draw: {},
+          setDraw: () => {},
+          saving: { plan: { currency: 'GBP' }, target: { currency: 'USD' } },
+        }}
       >
         <SessionProvider session={{ user: { data: { access_token: {} } } }}>
           <TransactionContext.Provider
             value={{ transaction: {}, setTransaction: () => {} }}
           >
-            <SavingSummary />
+            <QueryClientProvider client={queryClient}>
+              <SavingSummary />
+            </QueryClientProvider>
           </TransactionContext.Provider>
         </SessionProvider>
       </DrawContext.Provider>,
