52239270d2b61959e6ef9dc5d1ceaa747a0d6e20
abhishandy <abhishek.sandy@gmail.com>
2023-09-24 20:06:13 -0400

    test: modal content

--- components/atoms/Modal/content.test.js
+++ components/atoms/Modal/content.test.js
@@ -1,27 +1,73 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ContentModal from './content';
 import { SessionProvider } from 'next-auth/react';
 import { Dialog, Transition } from '@headlessui/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 describe('ContentModal', () => {
   let component;
   beforeEach(() => {
+    const queryClient = new QueryClient();
     const res = render(
-      <SessionProvider session={{ user: { name: 'John Doe' } }}>
-        <Transition appear show={true}>
-          <Dialog onClose={() => {}}>
-            <ContentModal>
-              <div>Content</div>
-            </ContentModal>
-          </Dialog>
-        </Transition>
-      </SessionProvider>,
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider
+          session={{
+            user: { name: 'John Doe', data: { access_token: 'at123' } },
+          }}
+        >
+          <Transition appear show={true}>
+            <Dialog onClose={() => {}}>
+              <ContentModal title={'TestTitle'} tabs />
+            </Dialog>
+          </Transition>
+        </SessionProvider>
+      </QueryClientProvider>,
     );
     component = res.container;
+
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
   });
 
   test('should render ContentModal component', () => {
     expect(component).toMatchSnapshot();
+  });
+
+  test('should render ContentModal component with title', () => {
+    const title = screen.queryByText('TestTitle');
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('should render email tab', async () => {
+    const emailLabel = await screen.findAllByText("Adresse e-mail d'un ami");
+    expect(emailLabel).toHaveLength(2);
+  });
+
+  test('should render phone tab', async () => {
+    fireEvent.click(screen.getByText('Phone'));
+    const phoneLabel = await screen.findAllByText(
+      "Numéro de téléphone d'un ami",
+    );
+    expect(phoneLabel).toHaveLength(2);
+  });
+
+  test('should submit form with email', async () => {
+    fireEvent.click(screen.getByText('Envoyer'));
+  });
+
+  test('should submit form with phone', async () => {
+    fireEvent.click(screen.getByText('Phone'));
+    fireEvent.click(screen.getByText('Envoyer'));
+  });
+
+  test('should add friend', async () => {
+    fireEvent.click(screen.getByText('+ Ajouter un ami'));
   });
 });
