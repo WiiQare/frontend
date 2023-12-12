5a22553b0d5ce7b001909958e74d9788fcf5366e
Peter NDENGO <frdrcpeter@gmail.com>
2023-12-12 18:29:58 +0100

    Set true for Kyc

--- components/atoms/Card/Saving/index.js
+++ components/atoms/Card/Saving/index.js
@@ -335,9 +335,9 @@ const SavingCard = ({ title, img, month = 12 }) => {
                                       fill="currentColor"
                                     >
                                       <path
-                                        fillRule="" evenodd"
-                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                      clipRule=""evenodd"
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                        clipRule="evenodd"
                                       />
                                     </svg>
 
@@ -388,9 +388,9 @@ const SavingCard = ({ title, img, month = 12 }) => {
                                       fill="currentColor"
                                     >
                                       <path
-                                        fillRule="" evenodd"
-                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                      clipRule=""evenodd"
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                        clipRule="evenodd"
                                       />
                                     </svg>
 
@@ -443,9 +443,9 @@ const SavingCard = ({ title, img, month = 12 }) => {
                                       fill="currentColor"
                                     >
                                       <path
-                                        fillRule="" evenodd"
-                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                      clipRule=""evenodd"
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                        clipRule="evenodd"
                                       />
                                     </svg>
 
