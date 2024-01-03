2db3577599d9ed1c56c2e2d1bce6e12ba3b54f8b
abhishandy <abhishek.sandy@gmail.com>
2024-01-02 21:44:30 -0500

    check auth

---
+++ pages/api/authologic/check.test.js
@@ -0,0 +1,67 @@
+import handler from "./check";
+
+describe("handler", () => {
+    it("should fetch data and send response", async () => {
+        const req = {
+            body: {
+                id: "123",
+            },
+        };
+
+        const res = {
+            send: jest.fn(),
+        };
+
+        global.fetch = jest.fn().mockResolvedValueOnce({
+            json: jest.fn().mockResolvedValueOnce({ data: "test data" }),
+        });
+
+        await handler(req, res);
+
+        expect(global.fetch).toHaveBeenCalledWith(
+            "https://sandbox.authologic.com/api/conversations/123",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/vnd.authologic.v1.1+json",
+                    Accept: "application/vnd.authologic.v1.1+json",
+                    Authorization: "Basic d2lpcWFyZTpXUjI2aldjZVNWVzNJTkQwejFCTGQwSG4=",
+                },
+            }
+        );
+
+        // expect(res.send).toHaveBeenCalledWith({ data: "test data" });
+    });
+
+    it("should log error", async () => {
+        const req = {
+            body: {
+                id: "123",
+            },
+        };
+
+        const res = {
+            send: jest.fn(),
+        };
+
+        global.fetch = jest.fn().mockRejectedValueOnce(new Error("fetch error"));
+
+        console.log = jest.fn();
+
+        await handler(req, res);
+
+        expect(global.fetch).toHaveBeenCalledWith(
+            "https://sandbox.authologic.com/api/conversations/123",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/vnd.authologic.v1.1+json",
+                    Accept: "application/vnd.authologic.v1.1+json",
+                    Authorization: "Basic d2lpcWFyZTpXUjI2aldjZVNWVzNJTkQwejFCTGQwSG4=",
+                },
+            }
+        );
+
+        // expect(console.log).toHaveBeenCalledWith(new Error("fetch error"));
+    });
+});
