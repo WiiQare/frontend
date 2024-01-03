55b443e0f3aa36b621909f8f9ab9a408792f3c8f
abhishandy <abhishek.sandy@gmail.com>
2024-01-02 21:10:36 -0500

    test: stripe molecule

--- components/molecules/Stripe/index.test.js
+++ components/molecules/Stripe/index.test.js
@@ -1,18 +1,26 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import StripePayment from '.';
+import { Provider } from 'react-redux';
+import { store } from '../../../redux/store';
+require('jest-fetch-mock').enableMocks();
+
 
 describe('ItemStripe', () => {
     it('should render the component stripe', () => {
 
+        fetch.mockResponse("{}");
+
         const amount = 20, senderId = 'sender', patientId = 'patient';
 
-        render(<StripePayment
-            amount={amount}
-            senderId={senderId}
-            patientId={patientId}
-        />)
+        render(<Provider store={store}>
+            <StripePayment
+                amount={amount}
+                senderId={senderId}
+                patientId={patientId}
+            />
+        </Provider>)
 
-        expect(screen.getByText('Détails du Patient')).toBeInTheDocument();
+        expect(screen.getByText('Loading...')).toBeInTheDocument();
     });
 })
